39393d05ca1a92034a1fa33818fbc7c27ca9b903
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-04-24 11:23:23 +0530

    create new table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,1 +1,7 @@
 /* Welcomr to the Address Book Service DataBase */
+
+create database AddressBookService
+
+create table AddressBook(ID int Primary key identity, FirstName varchar(50),LastName varchar(50),Address varchar(255),City varchar(50), State Varchar(50),Zip int, PhoneNumber varchar(20), Email varchar(50))
+
+select * from AddressBook
